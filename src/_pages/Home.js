c009e3dbe24e23427e2da0e657815cc29e72a040
Amit Mishra <amit.mishra@hloov.com>
2023-02-06 00:16:27 +0530

    Added Home Content

--- src/_pages/Home.js
+++ src/_pages/Home.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import './home.css'
+import {
+    ArrowStepInRight24Filled
+} from '@fluentui/react-icons'
 
 function Home() {
     return (
         <div className='_home'>
             <div className='_profile'>
                 <p className='_profile-fName'>Amit</p>
-                <p className='_profile-lName'>Mishra</p>
+                <p className='_profile-lName'>Mishra.</p>
+                <p className='_profile-Skill'></p>
                 <img className='_profileImage' src='amit-mishra.png' />
             </div>
-            <div className='_intro'></div>
+            <div className='_intro'>
+                <p className='hi'>Hi,</p>
+                <p className='hi_1'>I build things for <br />web.</p>
+                <p className='hi_2'>
+                    I'm a software engineer specializing in frontend technologies like react, redux, syncfusion, fluentUI, etc.
+                    Currently, I am focused on building accessible, human-centric AI led platform at <span>Hloov</span>.
+                </p>
+                <p className='hi_34'>
+                    <a className='hi_3' href="mailto: amitm780@gmail.com">E: amitm780@gmail.com</a>
+                    <a className='hi_4' href="tel:+918299488839">P: 8299488839</a>
+                </p>
+            </div>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -5,11 +5,33 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { FluentProvider, webDarkTheme } from '@fluentui/react-components';
 
+const myBrand = {
+  "10": "#FF6A00",
+  "20": "#FF6E07",
+  "30": "#FF730F",
+  "40": "#FF7716",
+  "50": "#FF7B1D",
+  "60": "#FF7F24",
+  "70": "#FF842C",
+  "80": "#FF8833",
+  "90": "#FF8C3A",
+  "100": "#FF9041",
+  "110": "#FF9549",
+  "120": "#FF9950",
+  "130": "#FF9D57",
+  "140": "#FFA15E",
+  "150": "#FFA666",
+  "160": "#FFAA6D"
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <FluentProvider theme={webDarkTheme}>
-      <App />
+      <div id='main-layout'>
+
+        <App />
+      </div>
     </FluentProvider>
   </React.StrictMode>
 );
